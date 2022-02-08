3e6ea3da2e102262521bf64b58082063bbcefe3b
villyGreen <vat.13@list.ru>
2022-02-09 01:08:55 +0700

    set route

--- SmartRoute/ViewController.swift
+++ SmartRoute/ViewController.swift
@@ -27,6 +27,7 @@ class ViewController: UIViewController {
     let routeButton: UIButton = {
            let button = UIButton(type: .system)
             button.setImage(UIImage(named: "route"), for: .normal)
+            button.tintColor = .red
            button.translatesAutoresizingMaskIntoConstraints = false
         return button
        }()
@@ -71,27 +72,30 @@ extension ViewController {
         stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -70).isActive = true
         stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         mapView.addSubview(routeButton)
+        routeButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        routeButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
         routeButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
         routeButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10).isActive = true
         routeButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 300).isActive = true
     }
     private func addTargets() {
         addRoutesButton.addTarget(self, action: #selector(addRoute), for: .touchUpInside)
-        routeButton.addTarget(self, action: #selector(ResetRoute), for: .touchUpInside)
-        resetButton.addTarget(self, action: #selector(route), for: .touchUpInside)
+        routeButton.addTarget(self, action: #selector(route), for: .touchUpInside)
+        resetButton.addTarget(self, action: #selector(ResetRoute), for: .touchUpInside)
         
     }
     
     private func addDirectional(startCoordinate: CLLocationCoordinate2D, endCoordinate: CLLocationCoordinate2D) {
         let request = MKDirections.Request()
         let startPlaceMark = MKPlacemark(coordinate: startCoordinate)
-        let endPlacemarl = MKPlacemark(coordinate: endCoordinate)
+        let endPlacemark = MKPlacemark(coordinate: endCoordinate)
         request.source = MKMapItem(placemark: startPlaceMark)
-        request.destination = MKMapItem(placemark: endPlacemarl)
-        request.transportType = .walking
+        request.destination = MKMapItem(placemark: endPlacemark)
+        request.transportType = .any
         request.requestsAlternateRoutes = true
         let directional = MKDirections(request: request)
         directional.calculate { (response, error) in
+            
             if error == nil {
                 var minRoutes = response?.routes[0]
                 for route in response!.routes {
@@ -101,16 +105,13 @@ extension ViewController {
                     self.mapView.addOverlay(minRoutes?.polyline as! MKOverlay)
                 }
             } else {
-                self.showAlert(title: "Ошибка", message: "Ошибка серверп", searchTextField: false, buttonTitle: "Ок") { (_) in
-                    print(" ")
+                self.showAlert(title: "Ошибка", message: "Ошибка сервера", searchTextField: false, buttonTitle: "Ок") { (_) in
+                    print(error!.localizedDescription)
                     return
                 }
             }
         }
-        
-        
     }
-    
     @objc private func addRoute() {
         showAlert(title: "Поиск места", message: "Введите адресс места", searchTextField: true, buttonTitle: "Добавить") { (text) -> () in
             CoordinateService.shared.findCoordinate(placeSearch: text) { (coordinates, error) in
@@ -134,19 +135,25 @@ extension ViewController {
         }
     }
     @objc private func ResetRoute() {
-        
+        mapView.removeAnnotations(mapView.annotations)
+        mapView.removeOverlays(mapView.overlays)
+        annotations = [MKPointAnnotation]()
+        resetButton.isHidden = true
+        routeButton.isHidden = true
     }
     
     @objc private func route() {
-        
+        for index in 0...annotations.count - 2 {
+            addDirectional(startCoordinate: annotations[index].coordinate, endCoordinate: annotations[index + 1].coordinate)
+        }
+        mapView.showAnnotations(annotations, animated: true)
     }
 }
-
 
 extension ViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
         let renderer = MKPolylineRenderer(overlay: overlay as! MKPolyline)
-        renderer.strokeColor = .red
+        renderer.strokeColor = .systemGreen
         return renderer 
     }
     
